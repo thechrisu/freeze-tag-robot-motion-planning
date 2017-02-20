01bf1875b2a2b31281930d9d5fa28f0aa59ee3a9
Anirudh <anirudhx5@gmail.com>
2017-02-20 20:06:49 +0000

    added shapely

--- solver/solver.py
+++ solver/solver.py
@@ -1,3 +1,6 @@
+from shapely.geometry import Point, LinearRing, LineString
+
+
 inp = "(0,1),(2,0),(3,5),(6,2),(9,0)#(1,2),(1,4),(3,4),(3,2);(8,1),(4,1),(4,4),(5,2)"
 
 def create_list_of_points(a):
@@ -7,10 +10,48 @@ def create_list_of_points(a):
 def parse(s):
     robots, obstacles = s.split('#')
     robots = create_list_of_points(robots)
+    robots = [Point(x, y) for x, y in robots]
+
     if obstacles:
         obstacles = obstacles.split(';')
-        obstacles = [create_list_of_points(i) for i in obstacles]
+        obstacles = [LinearRing(create_list_of_points(i)) for i in obstacles]
 
     return robots, obstacles
 
-print(parse(inp))
+class Solution(object):
+    """docstring for ."""
+
+    def __init__(self, inp):
+        robots, obstacles = parse(inp)
+        self.robots = robots
+        self.obstacles = obstacles
+        self.path = []
+
+    def answer(self):
+        start = self.robots[0]
+
+        for robot in self.robots[1:]:
+            self.reach_robot(start, robot)
+
+        return self.path
+
+    def reach_robot(self, start, robot):
+        p = LineString([(start.x, start.y), (robot.x, robot.y)])
+        intersections = []
+
+        for obstacle in self.obstacles:
+            intersection = p.intersection(obstacle)
+            if intersection:
+                intersections.append((intersection[0], obstacle))
+
+        if not intersections:
+            self.path.append(robot)
+            return
+
+        print(intersections)
+
+
+
+ans = Solution(inp)
+
+print(ans.answer())
